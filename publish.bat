ffe0c2f6fb333eaa5e0cb302d7b8b754b2fca524
최민경 <alsruddl1119@naver.com>
2025-02-27 18:22:41 +0900

    publish.bat 테스트

---
+++ publish.bat
@@ -0,0 +1,14 @@
+@echo
+
+if not exist "docs" (
+	mkdir docs
+)
+
+if not exist "docs\.nojekyll" (    
+	type nul > "docs\.nojekyll"
+)
+
+dotnet publish -c Release -o release
+xcopy release\wwwroot docs /E
+
+pause
