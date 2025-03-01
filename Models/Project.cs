0bc362ea13356ae938675485cfde015863b9d954
ChoiMingyeong <alsruddl1119@naver.com>
2025-03-01 23:56:46 +0900

    Project.cs 업데이트

--- Models/Project.cs
+++ Models/Project.cs
@@ -14,20 +14,7 @@ namespace DagaDatatable.Models
         public Dictionary<ProjectID, string> Projects { get; set; } = [];
     }
 
-    public readonly struct ProjectID
-    {
-        private ulong Value { get; }
-
-        public ProjectID(ulong value)
-        {
-            Value = value;
-        }
-
-        public static implicit operator ulong(ProjectID id) => id.Value;
-        public static explicit operator ProjectID(ulong value) => new(value);
-
-        public override string ToString() => Value.ToString();
-    }
+    
 
     public class Project
     {
