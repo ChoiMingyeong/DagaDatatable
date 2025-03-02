1f3771eb5911854f62c02b2621fc8bf3e544f087
ChoiMingyeong <alsruddl1119@naver.com>
2025-03-02 23:28:05 +0900

    Project.cs 업데이트

--- Models/Project.cs
+++ Models/Project.cs
@@ -2,20 +2,6 @@
 
 namespace DagaDatatable.Models
 {
-
-    public class Account
-    {
-        public AccountID Id { get; set; }
-
-        public string Email { get; set; } = string.Empty;
-
-        public string Name { get; set; } = string.Empty;
-
-        public Dictionary<ProjectID, string> Projects { get; set; } = [];
-    }
-
-    
-
     public class Project
     {
         public ProjectID Id { get; set; }
