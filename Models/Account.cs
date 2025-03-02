4d5874b69ec18a3c8ef3c975c77f357d23a84c80
ChoiMingyeong <alsruddl1119@naver.com>
2025-03-02 23:28:33 +0900

    Account.cs 만들기

---
+++ Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using DagaDatatable.TypeDefines;
+
+namespace DagaDatatable.Models
+{
+
+    public class Account
+    {
+        public AccountID Id { get; set; }
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public Dictionary<ProjectID, string> Projects { get; set; } = [];
+    }
+}
